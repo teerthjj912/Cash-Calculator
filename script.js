3bbaa2e804747abd89a713bfe35ea174afd5a7fd
Teerth <157950924+teerthjj912@users.noreply.github.com>
2024-08-22 22:35:11 +0530

    script.js

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ document.addEventListener('DOMContentLoaded', () => {
       const denominations = [2000, 500, 200, 100,50,20,10,5,2,1];
       const rowValue = cashInputs[index].value * denominations[index];
       cashTexts[index].textContent = rowValue.toFixed(0);
-      // Update respective result elements
   
 
       totalCash();
